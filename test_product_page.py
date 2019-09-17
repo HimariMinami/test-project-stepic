6a9cc1a365797dc1e15cabda3ac86d5cda90b605
HimariMinami <nasta30051997@gmail.com>
2019-09-17 16:12:15 +0300

    Add new test

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,8 @@
 import pytest
 from pages.product_page import ProductPage
+from pages.basket_page import BasketPage
+
+#для запуска: pytest -v -s --tb=line --language=en test_product_page.py
 
 #@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
 #                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -63,3 +66,11 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 	page = ProductPage(browser, link)
 	page.open()
 	page.go_to_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+	link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+	page = BasketPage(browser, link)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+	page.open()
+	page.go_to_basket_from_header_of_the_site()
+	page.is_basket_empty()
+	page.is_basket_not_empty()
