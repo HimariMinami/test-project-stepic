bf0f4c6a20134ae3c01872ffc7db4e4201a784fb
HimariMinami <nasta30051997@gmail.com>
2019-09-17 10:24:14 +0300

    Add BasePageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -11,3 +11,8 @@ class AddToCartLocators():
 	ALERT_CART_PRICE = (By.XPATH, "//div[3]/div[@class='alertinner ']") #уведомление о добавлении книги с её ценой
 	ALERT_CART_PRICE_NUM = (By.XPATH, "//div[@class='alertinner ']/p/strong") #цена книги в уведомлении
 	PRICE_BOOK = (By.XPATH, "//div[contains(@class, 'product_main')]/p[contains(@class, 'price_color')]") #цена книги
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    
