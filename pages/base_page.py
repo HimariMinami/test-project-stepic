aa9416cb679cdbd909ce9e26915c76cf358b8e94
HimariMinami <nasta30051997@gmail.com>
2019-09-17 16:06:37 +0300

    Add new method

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import math
 from .locators import BasePageLocators
+from .locators import BasketPageLocators
 
 class BasePage():
 	def __init__(self, browser, url, timeout=10):
@@ -59,3 +60,7 @@ class BasePage():
 
 	def should_be_login_link(self):
 		assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+	def go_to_basket_from_header_of_the_site(self):
+		button_basket = self.browser.find_element(*BasketPageLocators.BASKET_LINK)
+		button_basket.click()
