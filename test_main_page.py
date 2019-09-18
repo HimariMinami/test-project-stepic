db0aadba8024b5e02657e44486442de8a735a2ca
HimariMinami <nasta30051997@gmail.com>
2019-09-18 15:55:14 +0300

    Edit link

--- test_main_page.py
+++ test_main_page.py
@@ -5,12 +5,13 @@ import pytest
 
 #для запуска: pytest -v -s --tb=line --language=en test_main_page.py
 
+link = "http://selenium1py.pythonanywhere.com"
+
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
 	@pytest.mark.xfail
 	# не забываем передать первым аргументом self
 	def test_guest_can_go_to_login_page(self, browser):
-		link = "http://selenium1py.pythonanywhere.com"
 		page = MainPage(browser, link)			# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
 		page.open()								# открываем страницу
 		page.go_to_login_page()					
@@ -19,13 +20,11 @@ class TestLoginFromMainPage():
 		login_page.should_be_login_page()
 
 	def test_guest_should_see_login_link(self, browser):
-		link = "http://selenium1py.pythonanywhere.com"
 		page = MainPage(browser, link)
 		page.open()
 		page.should_be_login_link()
 		
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-	link = "http://selenium1py.pythonanywhere.com"
 	page = BasketPage(browser, link)
 	page.open()
 	page.go_to_basket_from_header_of_the_site()
--- test_product_page.py
+++ test_product_page.py
@@ -17,13 +17,14 @@ import time
 #                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
 #                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 
+linkNY = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+linkP = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+
 # файл с тест кейсами
 #def test_guest_can_add_product_to_basket(browser, link):
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-	#link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-	page = ProductPage(browser, link)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+	page = ProductPage(browser, linkNY)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
 	page.open()							# открываем страницу
 	page.should_be_cart_button()
 	page.add_to_cart()
@@ -35,46 +36,40 @@ def test_guest_can_add_product_to_basket(browser):
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-	page = ProductPage(browser, link)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+	page = ProductPage(browser, linkNY)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
 	page.open()
 	page.add_to_cart()
 	page.solve_quiz_and_get_code()
 	page.cant_see_success_message()
 
 def test_guest_cant_see_success_message(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-	page = ProductPage(browser, link)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+	page = ProductPage(browser, linkNY)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
 	page.open()
 	page.cant_see_success_message()
 
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-	page = ProductPage(browser, link)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+	page = ProductPage(browser, linkNY)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
 	page.open()
 	page.add_to_cart()
 	page.solve_quiz_and_get_code()
 	page.message_disappeared()
 
-def test_guest_should_see_login_link_on_product_page(browser):
-	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-	page = ProductPage(browser, link)
+def test_guest_should_see_login_link_on_product_page(browser):	
+	page = ProductPage(browser, linkP)
 	page.open()
 	page.should_be_login_link()
 
 @pytest.mark.need_review
 @pytest.mark.xfail
 def test_guest_can_go_to_login_page_from_product_page(browser):
-	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-	page = ProductPage(browser, link)
+	page = ProductPage(browser, linkP)
 	page.open()
 	page.go_to_login_page()
 
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-	page = BasketPage(browser, link)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+	page = BasketPage(browser, linkNY)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
 	page.open()
 	page.go_to_basket_from_header_of_the_site()
 	page.is_basket_empty()
@@ -94,9 +89,7 @@ class TestUserAddToBasketFromProductPage():
 
 	@pytest.mark.need_review
 	def test_user_can_add_product_to_basket(self, browser):
-		link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-		#link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-		page = ProductPage(browser, link)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+		page = ProductPage(browser, linkNY)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
 		page.open()							# открываем страницу
 		page.should_be_cart_button()
 		page.add_to_cart()
@@ -107,7 +100,6 @@ class TestUserAddToBasketFromProductPage():
 		page.ale_price_book_assert_price_book()
 
 	def test_user_cant_see_success_message(self, browser):
-		link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-		page = ProductPage(browser, link)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+		page = ProductPage(browser, linkNY)	# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
 		page.open()
 		page.cant_see_success_message()
