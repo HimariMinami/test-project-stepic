0de5549dc79a93698d35f70c756ddf1b46cd4d0e
HimariMinami <nasta30051997@gmail.com>
2019-09-18 14:31:08 +0300

    Add new method register_new_user

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
+from .locators import BasePageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -18,3 +19,13 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REG_FORM), "Registrarion form is not presented"
+
+    def register_new_user(self, email, password):
+        input1 = self.browser.find_element(*LoginPageLocators.REG_EMAIL)
+        input1.send_keys(email)
+        input2 = self.browser.find_element(*LoginPageLocators.REG_PASSWORD)
+        input2.send_keys(password)
+        input3 = self.browser.find_element(*LoginPageLocators.REG_CONF_PASSWORD)
+        input3.send_keys(password)
+        button_reg = self.browser.find_element(*LoginPageLocators.BUTTON_REG)
+        button_reg.click()
